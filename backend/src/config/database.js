a0910456389de9f1a91f5c0acfc4cc0f50026d08
ZakyRizqiHasanain <zaky.rizqi.ft24@mail.umy.ac.id>
2026-08-02 17:08:12 +0700

    Use IPv4 Supabase Pooler for Vercel serverless functions

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -3,7 +3,7 @@ const path = require("path");
 require("dotenv").config({ path: path.join(__dirname, "../../.env") });
 
 
-const connectionString = process.env.DATABASE_URL || "postgresql://postgres:Akunbaru13*@db.utzrhmxfttvtqkepxipe.supabase.co:5432/postgres";
+const connectionString = process.env.DATABASE_URL || "postgresql://postgres.utzrhmxfttvtqkepxipe:Akunbaru13*@aws-0-ap-northeast-1.pooler.supabase.com:6543/postgres";
 
 const config = {
     connectionString,
